f8650bddfe658c2182c09530e843d639d149a625
XierHacker <xierhacker@163.com>
2017-12-22 16:43:14 +0800

    initial commit

--- data_processing.py
+++ data_processing.py
@@ -12,7 +12,7 @@ def file2str(filename):
         texts = inp.read().decode('gbk')
     # print(texts)
     sentences = texts.split('\r\n')  # 根据换行切分
-    print(len(sentences))
+    #print(len(sentences))
 
     # 将不规范的内容（如每行的开头）去掉
     def clean(s):
@@ -38,10 +38,20 @@ def split2sentences(str):
     sentences = re.split(u'[，。！？、‘’“”]/[bems]', str)
     return sentences
 
-#做成标签和字符对应元组,传入所有的sentence就行.
+#做成标签和字符对应元组,传入所有的sentences,得到每句sentence对应的word和labels
+#顺便去除了一些空样本
 def make_dataset(sentences):
+    print("there has ",len(sentences)," sentence")
+    datas=[]
+    labels=[]
     for sentence in sentences:
-        words_tags = re.findall('(.)/(.)', sentences[0])
+        #这个时候word_tags应该是这种形式:[('人', 'b'), ('们', 'e'), ('常', 's'), ('说', 's')]
+        words_tags = re.findall('(.)/(.)', sentence)
+        if words_tags:
+            words_tags=np.array(words_tags)
+            words=words_tags[:,0];  datas.append(words)
+            tags=words_tags[:,1]; labels.append(tags)
+    return datas,labels
 
 
 
@@ -49,6 +59,7 @@ if __name__ =="__main__":
     text=file2str(filename='data/msr_train.txt')
     sentences=split2sentences(text)
     #print('Sentences number:', len(sentences))
-    print('Sentence Example:\n', sentences[0])
-    words_tags = re.findall('(.)/(.)', sentences[0])
-    print(words_tags)
+    #print('Sentence Example:\n', sentences[0])
+    datas,labels=make_dataset(sentences)
+    print(len(datas),len(labels))
+    print(datas[0],labels[0])
