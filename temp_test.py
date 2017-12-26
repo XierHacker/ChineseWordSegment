58825eb6e8558dbf0ab31378a88151c0ea70dd87
XierHacker <xierhacker@163.com>
2017-12-26 09:29:22 +0800

    update dilated CNN

--- temp_test.py
+++ temp_test.py
@@ -1,5 +1,29 @@
-name="这里有一只猪"
-print(name)
+import os
+import numpy as np
+import tensorflow as tf
 
-name2=list(name)
-print(name2)
+value=np.array([[1,1,1],[2,2,2],[3,3,3],[4,4,4]])
+graph=tf.Graph()
+with graph.as_default():
+    embeddings=tf.Variable(initial_value=value,name="embeddings")
+
+    #一维ids
+    ids1=tf.Variable(initial_value=[1,2],name="ids")
+    vecs1 = tf.nn.embedding_lookup(params=embeddings, ids=ids1)
+
+    #二维ids,可以做为批处理来用
+    ids2=tf.Variable(initial_value=[[1,2],[2,1],[0,2]])
+    vecs2=tf.nn.embedding_lookup(params=embeddings,ids=ids2)
+
+    init_op=tf.global_variables_initializer()
+    init_op = tf.global_variables_initializer()
+
+with tf.Session(graph=graph) as sess:
+    sess.run(init_op)
+    embed= sess.run(embeddings)
+    vec1=sess.run(vecs1)
+    vec2=sess.run(vecs2)
+    print("embeddings:\n",embed)
+    print("vec1:\n",vec1)
+    print("vec2:\n",vec2)
+
