d0e595643eaf2bca41f8710f120ba15cfc72c62e
XierHacker <xierhacker@163.com>
2017-12-28 09:51:01 +0800

    1'th draft completed

---
+++ test.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from bilstm import BiLSTM
+
+#读数据
+df_test=pd.read_pickle(path="./dataset/msr/summary_test.pkl")
+X_test=np.asarray(list(df_test['X'].values))
+y_test=np.asarray(list(df_test['y'].values))
+
+
+#testing model
+bilstm=BiLSTM()
+accuracy=bilstm.pred(name="msr",X=X_test,y=y_test)
+print(accuracy)
