ae8935fcae0dc6658698ffbd975fe8d90369b519
XierHacker <xierhacker@163.com>
2017-12-29 11:10:14 +0800

    update BiLsSTM_CRF test process

--- bilstm_crf.py
+++ bilstm_crf.py
@@ -79,11 +79,11 @@ class BiLSTM_CRF():
             #concat togeter(forward information and backward information)
             # shape of h is [batch_size, max_time, cell_fw.output_size*2]
             h=tf.concat(values=[outputs_forward,outputs_backward],axis=2,name="h")
-            print("h.shape:",h.shape)
+            #print("h.shape:",h.shape)
 
             #reshape,new shape is [batch_size*max_time, cell_fw.output_size*2]
             h=tf.reshape(tensor=h,shape=[-1,2*self.hidden_units_num2],name="h_reshaped")
-            print("h.shape",h.shape)
+            #print("h.shape",h.shape)
 
             #fully connect layer
             w=tf.Variable(
@@ -95,7 +95,7 @@ class BiLSTM_CRF():
                 name="bias"
             )
             logits=tf.matmul(h,w)+b     #shape of logits:[batch_size*max_time, 5]
-            print("logit.shape",logits.shape)
+            #print("logit.shape",logits.shape)
 
 
             #change logits to the shape of [batch_size,max_sentence_size,5]
@@ -104,7 +104,7 @@ class BiLSTM_CRF():
                     shape=(-1,self.max_sentence_size,self.class_num),
                     name="logits_normal"
             )
-            print("logits_normal:",logits_normal.shape)
+            #print("logits_normal:",logits_normal.shape)
 
             #loss
             log_likelihood,transition_params=crf.crf_log_likelihood(
@@ -112,6 +112,7 @@ class BiLSTM_CRF():
                 tag_indices=self.y_p,
                 sequence_lengths=self.sequence_lengths
             )
+            self.trans_matrix=tf.Variable(initial_value=transition_params,name="trans_matrix")
             self.loss = tf.reduce_mean(-log_likelihood)
 
             #decode,shape of potentials=[batch_size, max_seq_len, num_tags]
@@ -127,7 +128,7 @@ class BiLSTM_CRF():
                 tf.reshape(decode_tags,[-1]),
                 tf.reshape(self.y_p, [-1])
             )
-            print("correct_prediction.shape:", correct_prediction.shape)
+            #print("correct_prediction.shape:", correct_prediction.shape)
 
             # accracy
             self.accuracy = tf.reduce_mean(
@@ -208,29 +209,37 @@ class BiLSTM_CRF():
         start_time = time.time()
         if y is None:
             with self.session as sess:
+                viterbi_sequences=[]
+                viterbi_scores=[]
                 # restore model
                 new_saver = tf.train.import_meta_graph("./models/"+name+"/my-model-10000.meta", clear_devices=True)
                 new_saver.restore(sess, "./models/"+name+"/my-model-10000")
+                graph = tf.get_default_graph()      #get graph
 
-                graph = tf.get_default_graph()
                 # get opration from the graph
-                pred_normal = graph.get_operation_by_name("pred_normal").outputs[0]
                 X_p = graph.get_operation_by_name("input_placeholder").outputs[0]
-                sequence_lengths=graph.get_operation_by_name("sequence_lengths").outputs[0]
-                pred = sess.run(
-                    fetches=pred_normal,
-                    feed_dict={X_p: X,sequence_lengths:np.full(shape=(X.shape[0],),fill_value=self.max_sentence_size)}
-                )
+                ligits_normal=graph.get_operation_by_name("logits_normal").outputs[0]
+                trans_matrix=graph.get_operation_by_name("trans_matrix").outputs[0]
+
+                sequence_lengths = np.full(shape=(X.shape[0],), fill_value=self.max_sentence_size)
+                logits,trans = sess.run(fetches=[logits_normal,trans_matrix],feed_dict={X_p: X})
+
+                for i in range(X.shape[0]):
+                    viterbi_sequence,viterbi_score=crf.viterbi_decode(score=logits[i],transition_params=trans)
+                    viterbi_sequences.append(viterbi_sequence)
+                    viterbi_scores.append(viterbi_score)
+
                 #compute time
                 duration=round((time.time()-start_time)/60,2)
                 print("this operation spends ",duration," mins")
-                return pred
+                return viterbi_sequences, viterbi_scores
         else:
             with self.session as sess:
                 # restore model
                 new_saver = tf.train.import_meta_graph("./models/"+name+"/my-model-10000.meta", clear_devices=True)
                 new_saver.restore(sess, "./models/"+name+"/my-model-10000")
                 graph = tf.get_default_graph()
+
                 # get opration from the graph
                 accuracy=graph.get_operation_by_name("accuracy").outputs[0]
                 X_p = graph.get_operation_by_name("input_placeholder").outputs[0]
--- test.py
+++ test.py
@@ -9,7 +9,6 @@ df_test=pd.read_pickle(path="./dataset/msr/summary_test.pkl")
 X_test=np.asarray(list(df_test['X'].values))
 y_test=np.asarray(list(df_test['y'].values))
 
-
 #testing model
 bilstm=BiLSTM()
 accuracy=bilstm.pred(name="msr",X=X_test,y=y_test)
